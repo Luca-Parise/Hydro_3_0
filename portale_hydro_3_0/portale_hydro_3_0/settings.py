3970e4a315565c7c629ae9998e3834332abbd31a
Jacopo <jacopo.fioretti@zilioenvironment.com>
2026-02-04 15:11:50 +0100

    added allowed host address

--- portale_hydro_3_0/portale_hydro_3_0/settings.py
+++ portale_hydro_3_0/portale_hydro_3_0/settings.py
@@ -48,7 +48,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.10.229"]
 
 
 # Application definition
