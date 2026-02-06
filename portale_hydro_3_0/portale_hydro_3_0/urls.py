9b76239db7d28465c93597ae9a1811e885a079fd
LucaParise <luca.parise@zilioenvironment.com>
2026-02-06 08:46:33 +0100

    Add custom error pages and fix image path in login template
    - Created custom 400, 403, 404, and 500 error pages with appropriate messages and illustrations.
    - Updated the image path in the login template to correct the filename format.
    - Modified the URL configuration to include default error handling views.

--- portale_hydro_3_0/portale_hydro_3_0/urls.py
+++ portale_hydro_3_0/portale_hydro_3_0/urls.py
@@ -15,13 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
 from django.urls import path, include
+from django.views import defaults as default_views
 from django.views.generic import RedirectView
+
+
+
 
 urlpatterns = [
     path("", RedirectView.as_view(url="/portale/home/", permanent=False)),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('portale/', include('portale.urls')),
-    
 ]
